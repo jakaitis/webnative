a3f96a032adb5b13f0c5818d8cf52581975610ec
Ben Moskowitz <benrito@gmail.com>
2012-01-21 18:20:30 -0800

    Added new version of template, cleaned up structure.

---
+++ _pm_special/js/external.js
@@ -0,0 +1,20 @@
+(function() {
+
+  define( [ "lib/jquery.min.js" ], function() {
+
+    require( [
+        "lib/jquery.qtip.pack.js",
+        "lib/jquery.infieldlabel.min.js",
+        "lib/jquery.tinyscrollbar.min.js",
+        "lib/jquery_002.js",
+        "lib/modernizr-1.5.min.js",
+      ], function() {
+
+        require([
+          "lib/jquery-ui.js"
+        ], function() {
+        } );
+
+    });
+  });
+})();
